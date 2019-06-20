fe98221d513032feba33be45e65af4895c70ddb2
Otto de Visser <otto@devisser.com>
2019-06-20 16:22:13 +0200

    idk

--- testen/seconderyTest/main.cpp
+++ testen/seconderyTest/main.cpp
@@ -1,31 +1,54 @@
 #include "hwlib.hpp"
 
-int main( void ){	
-   
-   namespace target = hwlib::target;
-   target::board board; 
-   
-   auto leds = hwlib::all(
-      board.led1, board.led2, board.led3, board.led4, 
-      board.led5, board.led6, board.led7, board.led8, board.led9 );
-   board.green.write(1);
-   board.green.flush();  
-   
-   
-   for(;;){
-      // while(!board.sw1.read()){
-         for (int i = 0; i < 20; i++)
-         {
-            leds.write(1); leds.flush();
-            hwlib::wait_ms(5);
-            leds.write(0); leds.flush();
-            hwlib::wait_ms(5);
-         }
-            leds.write(0); leds.flush();
-            hwlib::wait_ms(5);
-            leds.write(0); leds.flush();
-            hwlib::wait_ms(5);
-         
-      // }
-   }
+int main() {
+  namespace target = hwlib::target;
+
+  auto trancemitter = target::pin_out(target::pins::d50);
+  //   auto reciever = target::pin_in(target::pins::d51);
+  //   auto button = target::pin_in(target::pins::d40);
+  // auto led = target::pin_out( target::pins::d47 );
+  // auto scl = target::pin_oc( target::pins::scl );
+  // auto sda = target::pin_oc( target::pins::sda );
+
+  // auto i2c_bus = hwlib::i2c_bus_bit_banged_scl_sda( scl,sda );
+
+  // auto display = hwlib::glcd_oled( i2c_bus, 0x3c );
+
+  // display.clear();
+
+  // volatile bool pulseBit = false;
+  // volatile int tmp = 0;
+
+  // led.write(0); led.flush();
+
+  while (true) {
+    trancemitter.write(1);
+    hwlib::wait_ms(10);
+    trancemitter.write(0);
+    hwlib::wait_ms(10);
+  }
+
+  return 0;
 }
+
+// tmp = 0;
+// for (int i = 0; i < 200; i++)
+// {
+//     tmp += rx.read();
+// }
+// hwlib::cout << tmp << "\n";
+// hwlib::wait_ms(10);
+// if (tmp > 50)
+// {
+//     for( uint16_t i = 10; i < display.size.x - 10; i++ ){
+//         for( uint16_t y = 10; y < display.size.y - 10; y++ ){
+//             display.write( hwlib::xy( i, y ) );
+//         }
+//     }
+//     display.flush();
+// }
+// else
+// {
+//     display.clear();
+//     display.flush();
+// }
