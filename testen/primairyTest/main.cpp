1c4bf7d661a27fdc6fb2386dcae7fb324b4d0215
Otto de Visser <otto@devisser.com>
2019-07-01 17:22:49 +0200

    broadcast werkt!

--- testen/primairyTest/main.cpp
+++ testen/primairyTest/main.cpp
@@ -1,11 +1,119 @@
 #include "hwlib.hpp"
+#include "array"
+
+#define STARTBYTE 0b11011011
+#define STOPBYTE 0b10001001
+#define BROADCASTBYTE 0b00000011
+#define SENDBYTE 0b00001100
+#define ACKBYTE 0b00110000
+#define RESENDBYTE 0b11000000
+
+
+class wiwire{
+  private:
+  hwlib::target::pin_out txPin;
+  hwlib::target::pin_in rxPin;
+  void sendByte(char byte);
+  void waitForStartByte();
+  char readByte();
+  bool verify(const char * msg, int sizeMsg, char byte);
+  char checksum(const char * msg, int sizeMsg);
+  hwlib::target::pin_out testProbe;
+  
+  public:
+  wiwire(hwlib::target::pin_out txPin, hwlib::target::pin_in rxPin)
+  : 
+  txPin(txPin),
+  rxPin(rxPin),
+  testProbe(hwlib::target::pin_out(hwlib::target::pins::d46))
+  {};
+  int broadcast(const char * msg, const int & sizeMsg);
+  int send();
+  char blockRead(char &msg, int &size);
+};
+
+int wiwire::broadcast(const char * msg, const int & sizeMsg){
+  sendByte(STARTBYTE);
+  sendByte(BROADCASTBYTE);
+  sendByte(sizeMsg);
+  for (int i = 0; i < sizeMsg; i++)
+  {
+    sendByte(*(msg+i));
+  }
+  sendByte(checksum(msg, sizeMsg));
+  sendByte(STOPBYTE);
+  return 0;
+}
+
+char wiwire::checksum(const char * msg, int sizeMsg){
+  char tmp = *msg;
+  if(sizeMsg <= 1){
+    return tmp;
+  }
+  for (int i = 1; i < sizeMsg; i++)
+  {
+    tmp ^= msg[i];
+  }
+  return tmp;
+}
+
+void wiwire::sendByte(char byte){
+  testProbe.write(1); testProbe.flush();
+  for (int i = 0; i < 8; i++)
+  {
+    if((byte >> i) % 2){
+      txPin.write(1);txPin.flush();
+      hwlib::wait_us(1500);
+      txPin.write(0);txPin.flush();
+      hwlib::wait_us(500);
+    }
+    else{
+      txPin.write(1);txPin.flush();
+      hwlib::wait_us(500);
+      txPin.write(0);txPin.flush();
+      hwlib::wait_us(1500);
+    }
+     
+  }
+  testProbe.write(0); testProbe.flush();
+}
+
+char wiwire::blockRead(char &msg, int &size){
+  
+}
+
+
+// class wiwire{
+//   private:
+//   hwlib::target::pin_out txPin;
+//   public:
+//   wiwire(hwlib::target::pin_out txPin):
+//   txPin(txPin)
+//   {}
+
+//   int write(const char * msg){
+//     hwlib::cout << msg;   
+//     for (int i = 0; i < 12; i++)
+//     {
+//       for (int j = 0; j < 8; j++)
+//       {
+//         char tmp = *(msg+i);
+//       }
+      
+//       hwlib::cout << *(msg+i);
+//     }
+    
+//     return 0; 
+//   }
+
+// };
 
 int main() {
   hwlib::cout << " test/n";
   namespace target = hwlib::target;
 
   auto tx = target::pin_out(target::pins::d50);
-  // auto rx = target::pin_in(target::pins::d51);
+  auto rx = target::pin_in(target::pins::d51);
   // auto button = target::pin_in( target::pins::d40 );
   // auto led = target::pin_out( target::pins::d47 );
   // auto scl = target::pin_oc( target::pins::scl );
@@ -24,24 +132,49 @@ int main() {
   // bool status = false;
   // int tmp = 0;
 
-  while (true) {
-    tx.write(0);
-    hwlib::wait_us(900);
-    tx.write(1);
-    hwlib::wait_us(3000);
-    tx.write(0);
-    hwlib::wait_us(900);
-    tx.write(1);
-    hwlib::wait_us(900);
-    tx.write(0);
-    hwlib::wait_us(900);
-    tx.write(1);
-    hwlib::wait_us(900);
-    hwlib::wait_ms(5000);
+  wiwire wire(tx, rx);
+  int sizeMsg = 12;
 
+  const char msg[12] = "Hello world";
+
+  while (true) {
+    
+    wire.broadcast(msg, sizeMsg);
+    hwlib::wait_ms(500);
+  //   for (int i = 0; i < 100; i++)
+  //   {
+  //     tx.write(1); tx.flush();
+  //   hwlib::wait_ms(1);
+  //   tx.write(0); tx.flush();
+  //   hwlib::wait_ms(1);
+  //   }
+    
+  //   tx.write(1); tx.flush();
+  //   hwlib::wait_us(1500);
+  //   tx.write(0); tx.flush();
+  //   hwlib::wait_us(500);
+  
+  // for (int i = 0; i < 100; i++)
+  //   {
+  //     tx.write(1); tx.flush();
+  //   hwlib::wait_ms(1);
+  //   tx.write(0); tx.flush();
+  //   hwlib::wait_ms(1);
+  //   }
+    
+  //   tx.write(1); tx.flush();
+  //   hwlib::wait_us(500);
+  //   tx.write(0); tx.flush();
+  //   hwlib::wait_us(1500);
+
+  //   tx.write(1); tx.flush();
+  //   hwlib::wait_us(500);
+  //   tx.write(0); tx.flush();
+  //   hwlib::wait_us(1500);
   }
 
   return 0;
+
 }
 
 // tmp = 0;
--- testen/seconderyTest/main.cpp
+++ testen/seconderyTest/main.cpp
@@ -1,29 +1,34 @@
 #include "hwlib.hpp"
 
-class pin_in_5 : public hwlib::target::pin_in{
+class pin_in_x : public hwlib::target::pin_in{
   private:
-  int counter_5 = 0;
+  uint8_t counter_x = 0;
   public:
-  pin_in_5(due::pins pin): hwlib::target::pin_in(pin){};
-  bool read_10(){
-    counter_5 = 0;
-    for (int i = 0; i < 50; i++)
+  pin_in_x(due::pins pin): hwlib::target::pin_in(pin){};
+  uint8_t read_x(int times){
+    counter_x = 0;
+    for (int i = 0; i < times; i++)
     {
       hwlib::wait_us(1);
-      counter_5 += read();
+      counter_x += read();
     }
     
-    return counter_5 < 45 ? true : false;
+    return counter_x; // return confidance
   }
 };
+
+
 
 int main() {
   namespace target = hwlib::target;
 
-  auto rx = pin_in_5(target::pins::d50);
+  auto rx = pin_in_x(target::pins::d50);
   //   auto reciever = target::pin_in(target::pins::d51);
   //   auto button = target::pin_in(target::pins::d40);
-  auto led = target::pin_out( target::pins::d53 );
+  auto led = target::pin_out( target::pins::d46 );
+  auto pulse = target::pin_out( target::pins::d47 );
+  auto high = target::pin_out( target::pins::d48 );
+  auto low = target::pin_out( target::pins::d49 );
   // auto scl = target::pin_oc( target::pins::scl );
   // auto sda = target::pin_oc( target::pins::sda );
 
@@ -38,20 +43,90 @@ int main() {
   // int errAllowance = 10;
 
   led.write(1); led.flush();
-
+  // uint64_t counter = 0;
+  int highLow = 0;
+  bool last = false;
+  bool pulseSpeed = false;
+  int sizeBuffer = 10;
+  char rollingBuffer[sizeBuffer];
+  int buffCounter = 0;
+  for (int i = 0; i < sizeBuffer; i++)
+    {
+      rollingBuffer[i] = rx.read();
+    }
   while (true) {
+    //sync:
+    // int size = 1000;
+    // uint8_t array[size];
+     int readValue = rx.read();
+    rollingBuffer[buffCounter] = readValue;
+    int tmp = 0;
+    for (int i = 0; i < sizeBuffer; i++)
+    {
+      tmp += rollingBuffer[i];
+    }
+    if (buffCounter < sizeBuffer - 1)
+    {
+      buffCounter++;
+    }
+    else
+    {
+      buffCounter = 0;
+    }
 
+    pulse.write(pulseSpeed); pulse.flush();
 
-    if (rx.read_10())
+    pulseSpeed = !pulseSpeed;
+    
+    if (tmp <= 2)
     {
       led.write(1); led.flush();
-      hwlib::wait_us(50);
+      if (!last)
+      {
+        if(highLow < 18){
+          low.write(1); low.flush();
+        }
+        if(highLow > 35){
+          high.write(1); high.flush();
+        }
+        highLow = 0;
+      }
+      highLow++;
+      last = true;
+      
     }
     else{
-    led.write(0); led.flush();
-    hwlib::wait_us(50);
+      led.write(0); led.flush();
+      last = false;
+      high.write(0); high.flush();
+      low.write(0); low.flush();
     }
+    
+    
+    
+    // for (int i = 0; i < size; i++)
+    // {
+    //     hwlib::cout << array[i] << " ";
+    // }
+    
 
+
+    // if (rx.read_x())
+    // {
+    //   led.write(1); led.flush();
+    //   hwlib::wait_us(50);
+    //  if (!tmp){
+    //     hwlib::cout << counter++ << "\n";
+    //     tmp = true;
+    //   }
+    // }
+    // else{
+    // led.write(0); led.flush();
+    // hwlib::wait_us(50);
+    
+    //    tmp = false;
+    // }
+  
   }
   return 0;
   
